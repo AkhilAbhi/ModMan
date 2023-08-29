f70d1185f0d652bb42441e43d9b3e706c89d4c6b
AkhilAbhi <66011589+AkhilAbhi@users.noreply.github.com>
2023-08-29 19:32:28 +0530

    test

---
+++ modules/chek.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+from . import group
+
+async def chekall(client,message):
+    name = str(message.chat.type)
+    if(name == "ChatType.PRIVATE"):
+        print('private chat')
+    elif(name == "ChatType.GROUP"):
+        # print("group chat")
+        await group.getMessage(client,message)
+    elif(name == "ChatType.CHANNEL"):
+        print("chanel chat")
+    else:
+        print('somthin went worng')
